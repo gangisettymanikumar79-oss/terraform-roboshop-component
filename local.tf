c499d898944a2e5cca1c755d185cf9c685692b8a
.email.com <gangisettymani98@gmail.com>
2026-09-07 20:02:33 +0530

    update

---
+++ local.tf
@@ -0,0 +1,20 @@
+locals {
+  ami_id = data.aws_ami.joindevops.id
+  sg_id = data.aws_ssm_parameter.sg_id.value
+  backend_alb_listener_arn = data.aws_ssm_parameter.backend_alb_listener_arn.value
+  frontend_alb_listener_arn =data.aws_ssm_parameter.frontend_alb_listener_arn.value
+  private_subnet_id=data.aws_ssm_parameter.private_subnet_ids.value
+  common_name ="${var.project}-${var.environment}-${var.component}"
+  alb_listener_arn = var.component == "frontend" ? local.frontend_alb_listener_arn : local.backend_alb_listener_arn
+  host_header = var.component == "frontend" ? "${var.project}-${var.environment}.${var.domain_name}" : "${var.component}.backend-alb-${var.environment}.${var.domain_name}"
+  vpc_id = data.aws_ssm_parameter.vpc_id .value
+  common_tags = {
+    project     = var.project
+    environment = var.environment
+    terraform   = "true"
+  } 
+
+
+}
+
+
---
+++ variable.tf
@@ -0,0 +1,29 @@
+variable "project" {
+    default = "roboshop"
+
+}
+
+variable "environment" {
+    default = "dev"
+}
+
+variable "app_version" {
+     default = "v3"
+  
+}
+
+variable "domain_name" {
+    default = "manikumar.online"
+  
+}
+
+variable "component" {
+   type = string
+  
+}
+
+variable "rule_priority" {
+  #  type = string
+  
+}
+
