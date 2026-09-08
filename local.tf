1ea9c6fc6dfbeb71d12e6cee1099fe01e26cb766
.email.com <gangisettymani98@gmail.com>
2026-09-08 11:11:49 +0530

    update

--- local.tf
+++ local.tf
@@ -2,8 +2,8 @@ locals {
   ami_id = data.aws_ami.joindevops.id
   sg_id = data.aws_ssm_parameter.sg_id.value
   backend_alb_listener_arn = data.aws_ssm_parameter.backend_alb_listener_arn.value
-  frontend_alb_listener_arn =data.aws_ssm_parameter.frontend_alb_listener_arn.value
-  private_subnet_id=data.aws_ssm_parameter.private_subnet_ids.value
+  frontend_alb_listener_arn = data.aws_ssm_parameter.frontend_alb_listener_arn.value
+  private_subnet_id = split(",", data.aws_ssm_parameter.private_subnet_ids.value)[0]
   common_name ="${var.project}-${var.environment}-${var.component}"
   alb_listener_arn = var.component == "frontend" ? local.frontend_alb_listener_arn : local.backend_alb_listener_arn
   host_header = var.component == "frontend" ? "${var.project}-${var.environment}.${var.domain_name}" : "${var.component}.backend-alb-${var.environment}.${var.domain_name}"
